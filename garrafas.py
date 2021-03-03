3282159dfefb9c09771669d7ac27c1788e651fb1
Maltinhoo <malthusim5@gmail.com>
2021-03-03 13:07:30 -0300

    organizando as garrafas

--- garrafas.py
+++ garrafas.py
@@ -8,3 +8,12 @@ resposta = ""
 for q in range(1, quantidade+1):
     cont = float(input(f"Litros da garrafa %i :" %q))
     garrafas.append(cont)
+
+garrafas.sort(reverse=True)
+for garrafa in garrafas:
+    if galao-garrafa >=0:
+        galao = galao - garrafa
+        resposta = resposta + f"%.2fL, " % garrafa
+
+print(f"Usamos as garrafas de: %s" %resposta)
+print(f"Sobrou: %2fL" %galao)
