78514635281c2ac9cb133f419de26a3c9e4ce5bf
Maltinhoo <malthusim5@gmail.com>
2021-03-03 12:52:48 -0300

    laço para criar a lista de garrafas

--- garrafas.py
+++ garrafas.py
@@ -5,3 +5,6 @@ quantidade = int(input("Quantidade de Garrafas: "))
 garrafas = []
 resposta = ""
 
+for q in range(1, quantidade+1):
+    cont = float(input(f"Litros da garrafa %i :" %q))
+    garrafas.append(cont)
